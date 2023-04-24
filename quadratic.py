0bf8987245ddcf9711c8406f6da430a4282b9fc9
Roshni-17 <roshni.r.k.mangalam@gmail.com>
2023-04-24 17:07:07 +0530

    Added quadratic file

---
+++ quadratic.py
@@ -0,0 +1,8 @@
+a=int(input("enter the value of a = "))
+b=int(input("enter the value of b = "))
+c=int(input("enter the value of c ="))
+k=(b**2)-(4*a*c)
+d=k**0.5
+x1=((-b+d)/(2*a))
+x2=((-b-d)/(2**a))
+print("The roots are ",x1,x21)
