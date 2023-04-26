a31acb677bf20b90265bd96d08943bb948d22cf9
Roshni-17 <roshni.r.k.mangalam@gmail.com>
2023-04-26 14:04:28 +0530

    Added python file

--- problem4.py
+++ problem4.py
@@ -1,24 +1,54 @@
-for i in range(1, 5):
-    for j in range(1, 4):
-        print(1, end=" ")
-    print()
+# for i in range(1, 5):
+#     for j in range(1,4):
+#         print(1, end=" ")
+#     print()
+#
+# for i in range(1, 5):
+#     for j in range(1,4):
+#         print('*', end=" ")
+#     print()
+#
+#
+# for i in range(1, 5):
+#     for j in range(1,4):
+#         print(i, end=" ")
+#     print()
+#
+# for i in range(1, 5):
+#     for j in range(1,4):
+#         print(j, end=" ")
+#     print()
+#
+# for i in range(1, 5):
+#     for j in range(1,4):
+#         print(j+4, end=" ")
+#     print()
 
-for i in range(1, 5):
-    for j in range(1, 4):
-        print('*', end=" ")
-    print()
 
-for i in range(1, 5):
-    for j in range(1, 4):
-        print(i, end=" ")
-    print()
+# for i in range(1, 5):
+#     for j in range(i):
+#         print(j+1, end=" ")
+#     print()
+#
+# for i in range(1, 5):
+#     for j in range(i):
+#         print(i, end=" ")
+#     print()
+#
+# for i in range(1, 5):
+#     for j in range(i):
+#         print('*', end=" ")
+#     print()
 
-for i in range(1, 5):
-    for j in range(1, 4):
-        print(j, end=" ")
-    print()
+# for i in range(1, 5):
+#     for j in range(i, 5):
+#         print(i, end=" ")
+#     print()
 
-for i in range(1, 5):
-    for j in range(1, 4):
-        print(j + 4, end=" ")
+for i in range(1, 7):
+    for j in range(1,7):
+        if(i==j or i==6 or j==1):
+            print('*', end=" ")
+        else:
+            print(" ", end=" ")
     print()
