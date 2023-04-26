7b1ad60ccecbfb38fb1ffa462db2bf0a3ba3a6fb
Roshni-17 <roshni.r.k.mangalam@gmail.com>
2023-04-26 06:31:39 +0530

    Added leap year file

---
+++ leapyear.py
@@ -0,0 +1,11 @@
+year = int(input("enter a year"))
+if(year%4==0):
+    if(year%100==0):
+        if(year%400==0):
+            print("It is a leap year")
+        else:
+            print("Not a leap year")
+    else:
+        print("It is a leap year")
+else:
+    print("Not a leap year")
